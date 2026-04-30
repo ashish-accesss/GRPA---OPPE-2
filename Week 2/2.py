7c1dcb2cf92f09a01f996a1dc520cfe366012341
ashish-accesss <ashish11072005@gmail.com>
2026-04-30 23:38:41 +0530

    week 2 grpa 2

---
+++ Week 2/2.py
@@ -0,0 +1,11 @@
+# A single quote ' and a double quote "
+# A forward slash / and a backward slash \
+# Three single quotes ''' and three double quotes """
+# Double forward slash // and Double backward slash \\
+    
+    
+    
+output1=("A single quote \' and a double quote \"")
+output2=("A forward slash / and a backward slash \\")
+output3=("Three single quotes \'\'\' and three double quotes \"\"\"")
+output4=("Double forward slash // and Double backward slash \\\\")
