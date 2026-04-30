12b28f122cdeb09aa2c2f4e1c0a12219b437e6bb
ashish-accesss <ashish11072005@gmail.com>
2026-04-30 23:37:19 +0530

    week 1 grpa 1

---
+++ Week 2/1.py
@@ -0,0 +1,18 @@
+x1 = input()
+x2 = input()
+y1 = input()
+y2 = input()
+y3 = input()
+z = input()
+
+# swap the values of `x1` and `x2`
+
+# do a circular swap of `y1`, `y2` and `y3`  like y1 = y2, y2 = y3, y3 = y1 
+
+# create a new variable `a` with the value of `z`
+
+# delete the variable `z`
+x1,x2=x2,x1
+y1,y2,y3=y2,y3,y1
+a=z
+del z
